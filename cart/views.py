8d7ca672632e8a056d75b197007c64bfd8fa0670
Jalal <mamjalalkarimi@gmail.com>
2024-06-14 23:04:54 +0330

    set cart/views.py and add path cart in config/urls

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
-# Create your views here.
+from products.models import Product
+from .cart import Cart
+from .forms import CartAddForm
+
+
+def cart_detail_view(request):
+    cart = Cart(request)
+
+    for item in cart:
+        item['product_update_quantity_form'] = CartAddForm(initial={
+            'quantity': item['quantity'],
+            'inplace': True,
+        })
+    return render(request, 'cart/cart_detail.html', {
+        'cart': cart,
+    })
+
+
+@require_POST
+def cart_add_view(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddForm(request.POST)
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        quantity = cleaned_data['quantity']
+        cart.add(product, quantity, replace_current_quantity=cleaned_data['inplace'])
+
+    return redirect('product_list')
+
+
+def cart_remove_view(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+
+@require_POST
+def clear_cart(request):
+    cart = Cart(request)
+
+    if len(cart):
+        cart.clear()
+        messages.success(request, _('All products successfully removed from your cart'))
+    else:
+        messages.warning(request, _('Your cart is already empty'))
+
+    return redirect('product_list')
+
